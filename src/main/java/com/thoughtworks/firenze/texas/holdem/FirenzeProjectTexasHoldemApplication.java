ada94d079eb9bdf858695c6a850bdfa1299d7c8c
xiefei <feixie@thoughtworks.com>
2021-04-25 17:50:33 +0800

    init spring boot project

---
+++ src/main/java/com/thoughtworks/firenze/texas/holdem/FirenzeProjectTexasHoldemApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.firenze.texas.holdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirenzeProjectTexasHoldemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirenzeProjectTexasHoldemApplication.class, args);
+	}
+
+}
