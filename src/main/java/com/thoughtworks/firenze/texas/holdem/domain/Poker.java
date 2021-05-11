498393294175af48be97a288bbc889bd39edbe41
xiefei <feixie@thoughtworks.com>
2021-05-11 11:42:04 +0800

    add game public cards test

--- src/main/java/com/thoughtworks/firenze/texas/holdem/domain/Poker.java
+++ src/main/java/com/thoughtworks/firenze/texas/holdem/domain/Poker.java
@@ -21,15 +21,15 @@ public class Poker {
     Queue<Card> cards;
 
     public Poker() {
-        List<Card> cards = Stream.of(PokerValue.values())
+        List<Card> poker = Stream.of(PokerValue.values())
                                  .flatMap(value -> Stream.of(PokerType.values())
                                                          .map(type -> Card.builder()
                                                                           .value(value)
                                                                           .type(type)
                                                                           .build()))
                                  .collect(Collectors.toList());
-        Collections.shuffle(cards);
-        this.cards = new LinkedList<>(cards);
+        Collections.shuffle(poker);
+        this.cards = new LinkedList<>(poker);
     }
 
     public List<Card> deal(int count) {
--- src/test/groovy/com/thoughtworks/firenze/texas/holdem/domain/PokerTest.groovy
+++ src/test/groovy/com/thoughtworks/firenze/texas/holdem/domain/PokerTest.groovy
@@ -7,7 +7,7 @@ import com.thoughtworks.firenze.texas.holdem.domain.operation.Pet
 import spock.lang.Specification
 
 class PokerTest extends Specification {
-    def "should get public pokers in flop"() {
+    def "should get public pokers on different round in game"() {
         given:
         def players = [
                 PlayerBuilder.withDefault().name("A").build(),
@@ -23,6 +23,7 @@ class PokerTest extends Specification {
         game.players.forEach( {
             assert it.cards.size() == 2
         })
+        game.poker.cards.size() == 44
         when:
         game.next(new Pet())
         game.next(new Pet())
@@ -31,6 +32,25 @@ class PokerTest extends Specification {
         then:
         game.currentRound.name == RoundName.FLOP
         game.publicCards.size() == 3
+        game.poker.cards.size() == 41
+        when:
+        game.next(new Pet())
+        game.next(new Pet())
+        game.next(new Pet())
+        game.next(new Pet())
+        then:
+        game.currentRound.name == RoundName.TURN
+        game.publicCards.size() == 4
+        game.poker.cards.size() == 40
+        when:
+        game.next(new Pet())
+        game.next(new Pet())
+        game.next(new Pet())
+        game.next(new Pet())
+        then:
+        game.currentRound.name == RoundName.RIVER
+        game.publicCards.size() == 5
+        game.poker.cards.size() == 39
     }
 
     def "should init poker"() {
